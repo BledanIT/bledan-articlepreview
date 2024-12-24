c9d05fe50cbfcac93207602e811ff0657bce1630
BledanIT <bledan@gmail.com>
2024-12-24 20:05:36 +0100

    fix: image paths

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = () => {
   return (
     <main className='bg-white grid grid-cols-1 grid-rows-[auto_auto_auto] m-auto w-[30rem] h-fit rounded-xl drop-shadow-xl
     lg:grid-flow-col lg:grid-cols-[1fr_2fr] lg:grid-rows-[auto_auto] lg:w-[64rem]'>
-        <img src='src/assets/images/drawers.jpg' alt='drawers' className='object-cover object-top rounded-t-xl w-full h-80
+        <img src='images/drawers.jpg' alt='drawers' className='object-cover object-top rounded-t-xl w-full h-80
         lg:w-fit lg:h-full lg:rounded-none lg:rounded-l-xl lg:row-span-2'></img>
         <TextArea />
         <Author />
